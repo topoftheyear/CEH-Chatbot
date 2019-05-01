5ab7e33895ee849bb07b7a3b2a62d2e217cb798a
Aaron Pfarrer <aaron.pfarrer@ndus.edu>
2019-05-01 10:05:50 -0500

    Response changes

--- handlers/gift.py
+++ handlers/gift.py
@@ -40,22 +40,22 @@ class GiftHandler(MessageHandler):
         # positions 1-3 are gifts from a low price to a high price respectively.
         # position 4 is a funny gift idea.
         male_gift_ideas = [
-            " Socks are cool, everyone loves socks! Here are some I suggest!"
-            " Video games are pretty popular and can be inexpensive, how about this one?",
-            " Here are some Nikes that are pretty polular right now.",
-            " Everyone loves a good laugh, I think this would be pretty funny."
+            " Socks are cool, everyone loves socks! Here are some I suggest! --- https://www.amazon.com/Fruit-Loom-Heavy-Reinforced-Socks/dp/B005FYNXGO/ref=sr_1_3?keywords=male+socks&qid=1556570092&s=gateway&sr=8-3"
+            " Video games are pretty popular and can be inexpensive, how about this one? --- https://www.amazon.com/Mortal-Kombat-11-PlayStation-4/dp/B07L6KD1K3/ref=sr_1_3?crid=DDX0LRC1MFJO&keywords=mortal+kombat+11&qid=1556570169&s=gateway&sprefix=mortal+%2Caps%2C148&sr=8-3",
+            " Here are some Nikes that are pretty polular right now. --- https://www.amazon.com/Jordan-Rings-Royal-Black-White-Leather/dp/B06XYVP49L/ref=sr_1_299?keywords=mens+nike&qid=1556570277&s=gateway&sr=8-299",
+            " Everyone loves a good laugh, I think this would be pretty funny. --- https://www.amazon.com/Worlds-Farter-Father-T-Shirt-X-Large/dp/B00XU93ZK0/ref=sr_1_3?keywords=funny+gift+for+dad&qid=1556570309&s=gateway&sr=8-3"
         ]
         female_gift_ideas = [
-            " female low price",
-            " female medium price",
-            " female high price",
-            " female funny gift"
+            " Candles are always nice, How about this one. --- https://www.amazon.com/Yankee-Candle-Large-Home-Sweet/dp/B000WUFVR0/ref=sr_1_26?keywords=candle&qid=1556570330&s=gateway&sr=8-26",
+            " Flowers can be well priced, Here is one. --- https://www.amazon.com/Jackcsale-Romantic-Wedding-Valentines-Confession/dp/B01JFPKNRA/ref=sr_1_41?keywords=bouquet&qid=1556570451&s=gateway&sr=8-41",
+            " Jewelry can be up there is price, Here is an example. --- https://www.amazon.com/Princess-14K-Classic-Diamond-Engagement/dp/B00JRGDME4/ref=sr_1_3?keywords=diamond+ring&qid=1556570535&s=gateway&sr=8-3",
+            " Everyone loves a good laugh, I think this would be pretty funny. --- https://www.amazon.com/Mommin-Premium-Birthday-Friend-Present/dp/B01MUC28T3/ref=sr_1_11?keywords=funny+gift+for+mom&qid=1556570585&s=gateway&sr=8-11"
         ]
         sibling_gift_ideas = [
-            " sibling low price",
-            " sibling medium price",
-            " sibling high price",
-            " sibling funny"
+            " Keychains are cheap here are some I recommend. --- https://www.amazon.com/Disney-Princess-Keychain-Dangler-Figurine/dp/B01M758A0O/ref=sr_1_1?keywords=keychain+moana&qid=1556570767&s=gateway&sr=8-1",
+            " Siblings are always wanting food, and a free meal can be nice.",
+            " Everyone loves having fun, You can use this to have fun with your siblings. --- https://www.amazon.com/Nintendo-Switch-Neon-Red-Blue-Joy/dp/B01MUAGZ49/ref=sr_1_3?keywords=Nintendo+switch&qid=1556570893&s=gateway&sr=8-3",
+            " T-shirts can be funny, How about this? --- https://www.amazon.com/Have-A-Garbage-Day-T-shirt/dp/B07H98JLRD/ref=sr_1_9?keywords=garbage+t-shirt&qid=1556570930&s=gateway&sr=8-9"
         ]
 
         # if the none of the criteria is met, this is the default gift idea
